919c399b6c0560ba5a27789dcffa674c18aa6206
Oscar David Arbeláez <odarbelaeze@gmail.com>
2013-06-18 13:17:40 -0700

    Total cross sections computed for different energies using the Lennard-Jones potential

--- scatering/scatering.cc
+++ scatering/scatering.cc
@@ -1,18 +1,42 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
 #include "numerov.h"
 #include "bessel.h"
 
+double total_cross_section_lj (double E)
+{
+    double k = std::sqrt(E * ALPHA);
+    double r1, r2, u1, u2;
+    unsigned l_max = 7;
+
+    double K;
+    double tdl;
+    double delta_l;
+
+    double sigma_tot = 0.0;
+
+    for (int l = 0; l < l_max; ++l)
+    {
+        numerov::rse_lj(l, E, 5.0, 0.001, &r1, &u1, &r2, &u2, false);
+        K = (r1 * u2) / (r2 * u1);
+        tdl = (K * bessel::j(l, k * r1) - bessel::j(l, k * r2)) /
+              (K * bessel::n(l, k * r1) - bessel::n(l, k * r2)) ;
+        delta_l = std::atan(tdl);
+        sigma_tot += (2 * l + 1) * std::pow(std::sin(delta_l), 2);
+    }
+
+    return 4 * M_PI / (k * k) * sigma_tot;
+}
+
+
 int main(int argc, char const *argv[])
 {
-    double r1, r2, u1, u2;
+    std::cout << std::setprecision(9) << std::scientific;
 
-    numerov_rse([](double r) { return (r < 1.0)? - 2.0 : 0.0; },
-        1, 0.5, 2.0, 0.001, &r1, &u1, &r2, &u2, false);
-
-    std::cout << "# " << r1 << "   " << u1 << std::endl;
-    std::cout << "# " << r2 << "   " << u2 << std::endl;
-
-    std::cout << bessel::j(5, 1.5) << std::endl;
-    std::cout << bessel::n(5, 1.5) << std::endl;
+    for (double E = 0.2; E < 3.5; E += 0.02)
+        std::cout << E << "    " << total_cross_section_lj(E) << std::endl; 
 
     return 0;
 }
